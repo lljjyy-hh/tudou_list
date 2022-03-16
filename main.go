e1304a4b05f6578c618b9becdcb44867736ba821
lljjyy-hh <eyp560@qq.com>
2022-03-16 11:09:15 +0800

    :rocket: first server

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "11")
+	})
+	log.Printf("About to listen on 8080. Go to https://0.0.0.0:8080/")
+	// err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
+	err := http.ListenAndServeTLS(":8080", "./tls/server.crt", "./tls/server.key", nil)
+	log.Fatal(err)
 }
