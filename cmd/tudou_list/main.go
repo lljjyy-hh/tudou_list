32087dfe314f4d1a4656fbba1b2e52a2d8558ac1
lljjyy-hh <eyp560@qq.com>
2022-03-24 19:19:55 +0800

    :sparkles: 初始化服务器

--- cmd/tudou_list/main.go
+++ cmd/tudou_list/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+
+	"tudou_list/pkg/handler"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "11")
-	})
+	handler.Reg_handlers()
+
 	log.Printf("About to listen on 8080. Go to https://0.0.0.0:8080/")
 	// err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
-	err := http.ListenAndServeTLS(":8080", "./tls/server.crt", "./tls/server.key", nil)
+	err := http.ListenAndServeTLS(":8080", "../../tls/server.crt", "../../tls/server.key", nil)
+
 	log.Fatal(err)
 }
